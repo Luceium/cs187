439e3cd37e638796943090e71831cd3e3a834691
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-12-05 12:31:31 -0800

    selenium

---
+++ setup.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjs/14.0.0/math.min.js';
+document.head.appendChild(script);
