d786612ae7378147b0728c9b64a6082fffcbd6c5
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-12-08 15:46:31 -0800

    filter out spaces to reduce need for math.js

--- expression/eval.js
+++ expression/eval.js
@@ -1,5 +1,5 @@
-const expression = ${json}.expression.toLowerCase().trim();
-const correctAnswer = ${correctAnswer}.expression.toLowerCase().trim();
+const expression = ${json}.expression.toLowerCase().trim().split('').filter(c => c != ' ').join('');
+const correctAnswer = ${correctAnswer}.expression.toLowerCase().trim().split('').filter(c => c != ' ').join('');
 if (expression == correctAnswer) return true;
 var simplifiedChatbotAnswer = math.simplify(expression).toString();
 var simplifiedCorrectAnswer = math.simplify(correctAnswer).toString();
