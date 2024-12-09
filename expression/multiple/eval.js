4f72d7dea58971d49081f4cee144a32995cac3b5
Mau <mauriciom.curiel@gmail.com>
2024-12-09 00:09:43 -0800

    Update eval.js

--- expression/multiple/eval.js
+++ expression/multiple/eval.js
@@ -4,7 +4,7 @@ if (expressions.length != correctAnswers.length) throw Error();
 for (let i = 0; i < expressions.length; i++) {
     const expression = expressions[i].toLowerCase().trim().split('').filter(c => c != ' ').join('');
     const correctAnswer = correctAnswers[i].toLowerCase().trim().split('').filter(c => c != ' ').join('');
-    if (expression == correctAnswer) continue;
+    if (expression != correctAnswer && !math.symbolicEqual(expression, correctAnswer)) continue;
     throw Error();
 }
 return true;
