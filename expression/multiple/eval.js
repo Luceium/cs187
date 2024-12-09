9e84301e30288a7ef6ea720d55d5d1c328949a2c
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-12-08 17:36:03 -0800

    don't use math for multiple because it's only used for asymptouts which require equations instead of expressions

--- expression/multiple/eval.js
+++ expression/multiple/eval.js
@@ -5,8 +5,6 @@ for (let i = 0; i < expressions.length; i++) {
     const expression = expressions[i].toLowerCase().trim().split('').filter(c => c != ' ').join('');
     const correctAnswer = correctAnswers[i].toLowerCase().trim().split('').filter(c => c != ' ').join('');
     if (expression == correctAnswer) continue;
-    var simplifiedChatbotAnswer = math.simplify(expression).toString();
-    var simplifiedCorrectAnswer = math.simplify(correctAnswer).toString();
-    if (simplifiedChatbotAnswer != simplifiedCorrectAnswer && !math.equal(simplifiedChatbotAnswer, simplifiedCorrectAnswer)) throw Error();
+    throw Error();
 }
 return true;
